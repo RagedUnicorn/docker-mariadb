df11dc26556247bb7779ec8542f0a91df39c2dfb
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-02 00:20:53 +0200

    Cleanup docker-entrypoint script * Cleanup variable naming * Fix shellcheck findings - globing * Cleanup message for newly created user * Properly unset important environment variables * Fix shellcheck arithmetic finding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -o nounset
 
 mariadb_root_password="/run/secrets/com.ragedunicorn.mariadb.root_password"
 mariadb_app_user="/run/secrets/com.ragedunicorn.mariadb.app_user"
-mariadb_app_user_password="/run/secrets/com.ragedunicorn.mariadb.app_user_password"
+mariadb_app_password="/run/secrets/com.ragedunicorn.mariadb.app_user_password"
 
 create_data_dir() {
   echo "$(date) [INFO]: Creating data directory ${MARIADB_DATA_DIR} and setting permissions"
@@ -54,23 +54,28 @@ init() {
     create_data_dir
     create_run_dir
 
-    if [ -f "${mariadb_app_user}" ] && [ -f "${mariadb_app_user_password}" ] && [ -f "${mariadb_root_password}" ]; then
+    if [ $? -ne 0 ]; then
+      echo "$(date) [ERROR]: Failed to initialize mysqld - aborting...";
+      exit 1
+    fi
+
+    if [ -f "${mariadb_app_user}" ] && [ -f "${mariadb_app_password}" ] && [ -f "${mariadb_root_password}" ]; then
       echo "$(date) [INFO]: Found docker secrets - using secrets to setup mariadb"
 
       mariadb_root_password="$(cat ${mariadb_root_password})"
       mariadb_app_user="$(cat ${mariadb_app_user})"
-      mariadb_app_user_password="$(cat ${mariadb_app_user_password})"
+      mariadb_app_password="$(cat ${mariadb_app_password})"
     else
       echo "$(date) [INFO]: No docker secrets found - using environment variables"
 
-      mariadb_root_password="${MARIADB_ROOT_PASSWORD}"
-      mariadb_app_user="${MARIADB_APP_USER}"
-      mariadb_app_user_password="${MARIADB_APP_PASSWORD}"
-
-      unset "${MARIADB_ROOT_PASSWORD}"
-      unset "${MARIADB_APP_USER}"
-      unset "${MARIADB_APP_PASSWORD}"
+      mariadb_root_password="${MARIADB_ROOT_PASSWORD:?Missing environment variable MARIADB_ROOT_PASSWORD}"
+      mariadb_app_user="${MARIADB_APP_USER:?Missing environment variable MARIADB_APP_USER}"
+      mariadb_app_password="${MARIADB_APP_PASSWORD:?Missing environment variable MARIADB_APP_PASSWORD}"
     fi
+
+    unset "${MARIADB_ROOT_PASSWORD}"
+    unset "${MARIADB_APP_USER}"
+    unset "${MARIADB_APP_PASSWORD}"
 
     # mariadb will not start if standard database is not initialzed
     /usr/bin/mysql_install_db --user="${MARIADB_USER}" --basedir="${MARIADB_BASE_DIR}" --datadir="${MARIADB_DATA_DIR}"
@@ -92,19 +97,19 @@ init() {
       echo "$(date) [INFO]: Waiting for confirmation of MariaDB service startup, trying ${i}/${LOOP_LIMIT} ..."
       sleep 5
       mysql -uroot -e "status" > /dev/null 2>&1 && break
-      i=$(($i+1))
+      i=$((i + 1))
     done
 
     # create new user and grant remote access
     echo "$(date) [INFO]: Creating new user ${mariadb_app_user}"
-    sed -e "s/{{password}}/${mariadb_app_user_password}/g" \
+    sed -e "s/{{password}}/${mariadb_app_password}/g" \
       -e "s/{{user}}/${mariadb_app_user}/g" /home/user.sql | mysql -uroot;
 
     if [ $? -ne 0 ]; then
       echo "$(date) [ERROR]: Failed to create new user";
       exit 1
     else
-      echo "$(date) [INFO]: Created new app user"
+      echo "$(date) [INFO]: Created new user: ${mariadb_app_user}"
     fi
 
     mysql_secure
@@ -114,6 +119,7 @@ init() {
     mysqladmin -uroot -p"${mariadb_root_password}" shutdown
 
     set_init_done
+
     # start mariadb in foreground
     exec su-exec "${MARIADB_USER}" /usr/bin/mysqld_safe --basedir="${MARIADB_BASE_DIR}" --datadir="${MARIADB_DATA_DIR}"
   fi
